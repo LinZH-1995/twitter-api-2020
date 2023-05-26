ae7da9fcc981e4350dc3c0e99d72a856bf174708
LinZH-1995 <lin84610@gmail.com>
2023-05-26 11:00:25 +0800

    feat: separate admin/user signin

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,6 +5,18 @@ module.exports = {
     passport.authenticate('local', (err, user, info) => {
       if (err) return next(err)
       if (!user) return res.status(401).json({ status: 'error', message: info.message })
+      if (user.role === 'admin') return res.status(401).json({ status: 'error', message: 'admin禁止使用前台！' })
+      delete user.password
+      req.user = user
+      next()
+    })(req, res, next)
+  },
+
+  localStrategyAdminAuth: (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+      if (err) return next(err)
+      if (!user) return res.status(401).json({ status: 'error', message: info.message })
+      if (user.role === 'user') return res.status(401).json({ status: 'error', message: '一般使用者禁止使用後台！' })
       delete user.password
       req.user = user
       next()
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
+const admin = require('./modules/admin.js')
+
 const userController = require('../controllers/user-controller.js')
 
 const { errorHandler } = require('../middleware/error-handler.js')
 
-const { localStrategyAuth } = require('../middleware/auth.js')
+const { localStrategyAuth, localStrategyAdminAuth, jwtStrategyAuth } = require('../middleware/auth.js')
+
+router.post('/admin/signin', localStrategyAdminAuth, userController.signIn)
 
 router.post('/signup', userController.signUp)
 
 router.post('/signin', localStrategyAuth, userController.signIn)
 
-router.get('/', (req, res) => res.redirect('/api/signin'))
+router.use('/admin', jwtStrategyAuth, admin)
 
 router.use(errorHandler)
 
