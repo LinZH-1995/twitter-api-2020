082850a30060bb3bec3e54b1ce0c9e5f386f751f
LinZH-1995 <lin84610@gmail.com>
2023-05-30 15:43:05 +0800

    feat: add swagger and add admin swagger

--- app.js
+++ app.js
@@ -2,6 +2,30 @@ const express = require('express')
 const expSession = require('express-session')
 const connectFlash = require('connect-flash')
 const methodOverride = require('method-override')
+const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+const jsDocOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Twitter-api-2020',
+      version: '1.0.0',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {  //# arbitrary name for the security scheme
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT'
+        }          
+      }
+    }
+  },
+  apis: ['./routes/*.js', './routes/modules/*.js'], // files containing annotations as above
+}
+
+const swaggerSpec = swaggerJsdoc(jsDocOptions)
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -13,11 +37,6 @@ const passport = require('./config/passport.js')
 
 const app = express()
 const port = process.env.PORT || 3000
-
-// use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
-  // passport.authenticate('jwt', { ses...
-};
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
@@ -39,6 +58,13 @@ app.use((req, res, next) => {
 })
 
 app.use('/api', routes)
+
+app.get('/swagger/json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.send(swaggerSpec)
+})
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.listen(port, () => console.info(`Example app listening on port ${port}!`))
 
--- routes/index.js
+++ routes/index.js
@@ -12,10 +12,127 @@ const { errorHandler } = require('../middleware/error-handler.js')
 
 const { localStrategyAuth, localStrategyAdminAuth, jwtStrategyAuth, jwtStrategyAdminAuth } = require('../middleware/auth.js')
 
+/**
+ * @swagger
+ * /api/admin/signin:
+ *   post:
+ *     tags:
+ *       - Admin
+ *     summary: Admin SignIn
+ *     description: Return a JWT token for admin
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     requestBody:
+ *       description: Email and Password
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 default: 'root@example.com'
+ *               password:
+ *                 type: string
+ *                 default: '123456789'
+ *             required:
+ *               - email
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Success
+ *       401:
+ *         description: Unauthorized
+ */
 router.post('/admin/signin', localStrategyAdminAuth, userController.signIn)
 
+/**
+ * @swagger
+ * /api/signup:
+ *   post:
+ *     tags:
+ *       - SignIn/SignUp
+ *     summary: SignUp
+ *     description: Return a createdUser data
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     requestBody:
+ *       description: Name、Account、Eamil and Password
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 default: 'user10'
+ *               account:
+ *                 type: string
+ *                 default: '@user10'
+ *               email:
+ *                 type: string
+ *                 default: 'user10@example.com'
+ *               password:
+ *                 type: string
+ *                 default: '123456789'
+ *               checkPassword:
+ *                 type: string
+ *                 default: '123456789'
+ *             required:
+ *               - name
+ *               - account
+ *               - email
+ *               - password
+ *               - checkPassword
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Internal Server Error
+ */
 router.post('/signup', userController.signUp)
 
+/**
+ * @swagger
+ * /api/signin:
+ *   post:
+ *     tags:
+ *       - SignIn/SignUp
+ *     summary: SignIn
+ *     description: Return a JWT token for user
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     requestBody:
+ *       description: Email and Password
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 default: user1@example.com
+ *               password:
+ *                 type: string
+ *                 default: 123456789
+ *             required:
+ *               - email
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Success
+ *       401:
+ *         description: Unauthorized
+ */
 router.post('/signin', localStrategyAuth, userController.signIn)
 
 router.use('/admin', jwtStrategyAuth, jwtStrategyAdminAuth, admin)
